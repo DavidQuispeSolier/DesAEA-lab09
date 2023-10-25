3f168274857663869c55d717d3e0969557a01f76
Alumno <Alumno@LAB1504-20>
2023-10-24 21:55:35 -0500

    Agregar archivos de proyecto.

---
+++ Business/BProduct.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class BProduct
+    {
+        public List<Product> GetByName(string Name)
+        {
+
+            DProduct data = new DProduct();
+            var products = data.Get();
+
+            var result = products.Where(x => x.Name.Contains(Name)).ToList();
+            //lambda
+            //flecha
+
+
+            return result;
+        }
+
+        public List<Product> GetProduct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,103 @@
+﻿using Business;
+using Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+
+            BProduct bProduct = new BProduct();
+
+            //Listado de estudiante de entity
+            List<Product> products = bProduct.GetByName("");
+
+            //Convertir entidad a modelo
+
+            List<ProductModel> productModels = products.Select(x => new ProductModel
+            {
+                Id = x.Product_id,
+                Name = x.Name,
+                Price = x.Price,
+            }).ToList();
+
+            //Modelo es un listado de estudiantesModelo
+
+            return View(productModels);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductModel model)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
